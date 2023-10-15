dfdbf24b5b1bbc5ffb95e9c33273f06c3679161a
Muhammad Hamza Farooq <hamzaf14@gmail.com>
2023-10-16 00:54:17 +0500

    date selection fixed

--- cypress/e2e/task1.cy.js
+++ cypress/e2e/task1.cy.js
@@ -9,6 +9,8 @@ describe('Cowlar Task', () => {
     cy.get('#userEmail').type('qaengineer@cowlar.com')
     cy.get('label[for="gender-radio-1"]').click()
     cy.get('#userNumber').type('0123456789')
+    cy.get('#dateOfBirthInput').type('{selectall}').type('31 Oct 2023')
+    cy.get('#subjects-label').click()
     cy.get('#subjectsInput').type('Computer Science')
     cy.get('.subjects-auto-complete__option').first().click()
     cy.get('label[for="hobbies-checkbox-3"]').click()
@@ -26,8 +28,8 @@ describe('Cowlar Task', () => {
     cy.contains('Student Email').next('td').should('contain', 'qaengineer@cowlar.com')
     cy.contains('Gender').next('td').should('contain', 'Male')
     cy.contains('Mobile').next('td').should('contain', '0123456789')
-    cy.contains('Date of Birth').next('td').should('contain', '11 October,2023');
-    cy.contains('Subjects').next('td').should('contain', 'Computer Science');
+    cy.contains('Date of Birth').next('td').should('contain', '31 October,2023')
+    cy.contains('Subjects').next('td').should('contain', 'Computer Science')
     cy.contains('Hobbies').next('td').should('contain', 'Music')
     cy.contains('Address').next('td').should('contain', 'Address 1')
     cy.contains('State and City').next('td').should('contain', 'NCR Delhi')
