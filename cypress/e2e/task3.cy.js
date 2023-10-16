57badcbea335802953bd810ff651e3dd7b600abb
Muhammad Hamza Farooq <hamzaf14@gmail.com>
2023-10-16 06:50:33 +0500

    task3 changes

--- cypress/e2e/task3.cy.js
+++ cypress/e2e/task3.cy.js
@@ -1,15 +1,27 @@
-describe("Book Store Application Automation", () => {
-    beforeEach(() => {
-        cy.visit('https://demoqa.com/')
-      })
-    it("Should navigate to Understanding ECMAScript 6 book details", () => {
-      cy.contains("Book Store Application").click();
-      cy.contains("Book Store").should("be.visible");
-      cy.contains("Book Store").click();
-      cy.get(".main-header").should("have.text", "Book Store");
-      cy.contains("Understanding ECMAScript 6").click();
-      cy.contains("Title : Understanding ECMAScript 6").should("be.visible");
-      cy.contains("Author : Nicholas C. Zakas").should("be.visible");
+describe("Task3", () => {
+  it("Intercept and Verify Book API Response", () => {
+    cy.visit("/");
+
+    cy.intercept("GET", "/BookStore/v1/Book?ISBN=9781593277574").as("bookInfo");
+
+    cy.contains("Book Store Application").click();
+    cy.contains("Book Store").click();
+    cy.contains("Understanding ECMAScript 6").click();
+
+    cy.wait('@bookInfo').then((interception) => {
+      const response = interception.response;
+
+      expect(response.body).to.deep.equal({
+        "isbn": "9781593277574",
+        "title": "Understanding ECMAScript 6",
+        "subTitle": "The Definitive Guide for JavaScript Developers",
+        "author": "Nicholas C. Zakas",
+        "publish_date": "2016-09-03T00:00:00.000Z",
+        "publisher": "No Starch Press",
+        "pages": 352,
+        "description": "ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that E",
+        "website": "https://leanpub.com/understandinges6/read"
+      });
     });
   });
-  
+});
